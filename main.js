42a35a2b1aaa37220e55585dbe4fff3e1af0cbc5
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-06-28 22:08:14 +0100

    Create GameBoard() factory function

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ function Cell() {
 
   return { getToken, addToken };
 }
+
+function GameBoard() {
+  const board = [];
+
+  const initialize = () => {
+    for (let i = 0; i < 3; i++) {
+      board[i] = [];
+      for (let j = 0; j < 3; j++) {
+        board[i][j] = Cell();
+      }
+    }
+  };
+}
